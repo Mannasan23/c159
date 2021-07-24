491770181b62bae53f3a4c34dafeb0a55c5e2077
Manna <manna23.sandhu@gmail.com>
2021-07-24 13:03:11 +0530

    first commit

---
+++ js/Tour.js
@@ -0,0 +1,101 @@
+AFRAME.registerComponent("tour", {
+  init: function () {
+    this.placesContainer = this.el;
+    this.createCards()
+  },
+
+  createCards: function () {
+    const thumbNailsRef = [
+      {
+        id: "taj-mahal",
+        title: "Taj Mahal",
+        url: "./assets/thumbnails/taj_mahal.png",
+      },
+      {
+        id: "budapest",
+        title: "Budapest",
+        url: "./assets/thumbnails/budapest.jpg",
+      },
+
+      {
+        id: "eiffel-tower",
+        title: "Eiffel Tower",
+        url: "./assets/thumbnails/eiffel_tower.jpg",
+      },
+      {
+        id: "new-york-city",
+        title: "New York City",
+        url: "./assets/thumbnails/new_york_city.png",
+      },
+    ];
+    let prevoiusXPosition = -60;
+
+
+
+    for (var item of thumbNailsRef) {
+      const posX = prevoiusXPosition + 25;
+      const posY = 10;
+      const posZ = -40;
+      const position = { x: posX, y: posY, z: posZ };
+      prevoiusXPosition = posX;
+
+      // Border Element
+      var borderEl = this.createBorder(position,item.id);
+      // Thumbnail Element
+      var thumbnailEl = this.createThumbnail(item);
+      borderEl.appendChild(thumbnailEl);
+      // Title Text Element
+      var titleEl = this.createTitle(position,item);
+      borderEl.appendChild(titleEl);
+      this.placesContainer.appendChild(borderEl);
+    }
+  },
+
+  createBorder: function(position,id){
+    const entityEl = document.createElement("a-entity")
+
+    entityEl.setAttribute("id",id);
+    entityEl.setAttribute("visible",true);
+    entityEl.setAttribute("geometry",{
+      primitive:"ring",
+      radiusInner:9,
+      radiusOuter:10
+    });
+    entityEl.setAttribute("position",position);
+    entityEl.setAttribute("material",{
+      color:"blue",
+      opacity: 1
+    });
+    return entityEl;
+  },
+  createThumbnail: function(item){
+    const entityEl = document.createElement("a-entity")
+    
+    entityEl.setAttribute("visible",true);
+    entityEl.setAttribute("geometry",{
+      primitive:"circle",
+      radius:9,
+    }); 
+    entityEl.setAttribute("material",{src:item.url});
+
+    return entityEl;
+  },
+  createTitle: function(position,item){
+    const entityEl = document.createElement("a-entity")
+
+    entityEl.setAttribute("text",{
+      font:"exo2bold",
+      align:"center",
+      color:"#e65100",
+      width:70,
+      value:item.title
+    });
+    var pos = position;
+    pos.y-=30; // this was the issue. we are just changing y. now it is fixed. bye
+    entityEl.setAttribute("position",pos);
+    entityEl.setAttribute("visible",true);
+
+    return entityEl;
+  },
+  
+});
